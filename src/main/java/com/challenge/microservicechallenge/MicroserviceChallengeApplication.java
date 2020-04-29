d8a63b8831fb5729f00fcc487f25faec0b8fadf9
salvadorsuz <salvadorsuz@yahoo.es>
2020-04-29 18:06:45 +0200

    initial import

---
+++ src/main/java/com/challenge/microservicechallenge/MicroserviceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.microservicechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/microservicechallenge/MicroserviceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.microservicechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
